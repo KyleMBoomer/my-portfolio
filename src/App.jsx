fc4ca1c43fea9c2989cdf0eb3367f84a5596fa77
Kyle Mark Boomer <kylemarkboomer@Kyles-MacBook-Pro.local>
2024-09-05 14:31:33 -0500

    Rename Components to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './Header'
 import './App.css'
 
 function App() {
