0a6e852979921822c0cf33d031710acb85e53337
Kyle Mark Boomer <kylemarkboomer@Kyles-MacBook-Pro.local>
2024-09-05 14:26:05 -0500

    Initiate Header and Routes

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import {Link} from 'react-router-dom'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav>
+                <ul className="nav-links">
+                    <li>
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/resume">Resume</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact Me</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header 
