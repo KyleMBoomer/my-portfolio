13dce90d27cb2bc4303a1101e81966fafaa25188
Kyle Mark Boomer <kylemboomer@gmail.com>
2024-09-11 16:38:33 -0500

    Styled Project Cards and Project Carousel

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
         const glide = new Glide('.glide', {
             type: 'carousel',
             perView: 1,
-            gap: 10,
+            gap: 20,
         })
         glide.mount()
     }, [])
