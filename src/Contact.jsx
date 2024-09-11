98ed703d0e01c731698ce49f09beae073e357c18
Kyle Mark Boomer <kylemboomer@gmail.com>
2024-09-11 17:01:43 -0500

    Final Commit of the Day

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -9,7 +9,6 @@ const Contact = () => {
 
         emailjs.sendForm('service_12k5eko', 'contact_form', form.current, '7LNldnHOcUq6P-z3l7W07')
             .then((result) => {
-                console.log(result.text)
                 alert('Your email was sent!')
             }, (error) => {
                 console.log(error.text)
@@ -21,18 +20,16 @@ const Contact = () => {
 
     return (
         <div className="contact-wrapper">
-            <div className='contact-form-wrapper'>
+            <div className="contact-form-wrapper">
+                <div className="ripple"></div>
                 <h1>Let's Connect!</h1>
                 <form ref={form} onSubmit={sendEmail}>
                     <label>Name</label>
                     <input type="text" name="user_name" required />
-
                     <label>Email</label>
                     <input type="email" name="user_email" required />
-
                     <label>Message</label>
                     <textarea className="message-input" name="message" required />
-
                     <input className="button" type="submit" value="Send" />
                 </form>
             </div>
