b96d4a9f8ac3bae745f28b2a3968042e48b2bff5
Kyle Mark Boomer <kylemboomer@gmail.com>
2024-09-12 11:05:04 -0500

    Refactor/debug emailjs

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     const sendEmail = (e) => {
         e.preventDefault()
 
-        emailjs.sendForm('service_12k5eko', 'contact_form', form.current, '7LNldnHOcUq6P-z3l7W07')
+        emailjs.sendForm('service_4tzijdf', 'contact_form', form.current, 'aw_prqJ1KalF1-n97')
             .then((result) => {
                 alert('Your email was sent!')
             }, (error) => {
@@ -24,12 +24,12 @@ const Contact = () => {
                 <div className="ripple"></div>
                 <h1>Let's Connect!</h1>
                 <form ref={form} onSubmit={sendEmail}>
-                    <label>Name</label>
-                    <input type="text" name="user_name" required />
-                    <label>Email</label>
-                    <input type="email" name="user_email" required />
-                    <label>Message</label>
-                    <textarea className="message-input" name="message" required />
+                    <label htmlFor="user-name">Name</label>
+                    <input type="text" id="user-name" name="user_name" required />
+                    <label htmlFor="user-email">Email</label>
+                    <input type="email" id="user-email" name="user_email" required />
+                    <label htmlFor="user-message">Message</label>
+                    <textarea id="user-message"className="message-input" name="message" required />
                     <input className="button" type="submit" value="Send" />
                 </form>
             </div>
